edf7cfa41705e293e1d37e3320491f4cd88e0a2f
k0tsi0s <mynameiskotsios@gmail.com>
2021-04-01 13:22:03 +0100

    DnD - v1

---
+++ DnD - Campaign Dashboard v.1/DnD - Campaign Dashboard v.1/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace DnD___Campaign_Dashboard_v._1
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ DnD - Campaign Dashboard v.1/DnD - Campaign Dashboard v.1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DnD___Campaign_Dashboard_v._1.Startup))]
+namespace DnD___Campaign_Dashboard_v._1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
