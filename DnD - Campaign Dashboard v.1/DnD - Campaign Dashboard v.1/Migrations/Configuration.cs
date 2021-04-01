046533a0d0c7aa4d0dbe151af8920f76df64ce13
mason2998 <mason2998@hotmail.com>
2021-04-01 13:58:59 +0100

    Enable Migrations
    Added migrations to the program and attempted a login

---
+++ DnD - Campaign Dashboard v.1/DnD - Campaign Dashboard v.1/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+namespace DnD___Campaign_Dashboard_v._1.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<DnD___Campaign_Dashboard_v._1.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "DnD___Campaign_Dashboard_v._1.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(DnD___Campaign_Dashboard_v._1.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
