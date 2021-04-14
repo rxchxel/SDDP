9e4af606708bf6d935d01b83a615526a825194d8
Rachael Slack <57686060+rxchxel@users.noreply.github.com>
2021-04-14 15:40:05 +0100

    added more controllers
    more controllers added ready for views, code still needs implementing

---
+++ DnD - Campaign Dashboard v.1/DnD - Campaign Dashboard v.1/Controllers/MonsterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DnD___Campaign_Dashboard_v._1.Controllers
+{
+    public class MonsterController : Controller
+    {
+        // GET: Monster
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
