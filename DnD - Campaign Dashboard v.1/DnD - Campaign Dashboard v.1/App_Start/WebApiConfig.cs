234352d2a8313a5db9ad352eac376e8db2b3863a
mason2998 <mason2998@hotmail.com>
2021-04-14 12:53:12 +0100

    Added JSON formatters
    Added some formatting so that the api's we use to fetch data will be in JSON rather than XML

--- DnD - Campaign Dashboard v.1/DnD - Campaign Dashboard v.1/App_Start/WebApiConfig.cs
+++ DnD - Campaign Dashboard v.1/DnD - Campaign Dashboard v.1/App_Start/WebApiConfig.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
 
 namespace DnD___Campaign_Dashboard_v._1
 {
@@ -9,6 +12,12 @@ namespace DnD___Campaign_Dashboard_v._1
     {
         public static void Register(HttpConfiguration config)
         {
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
+            var settings = config.Formatters.JsonFormatter.SerializerSettings;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            settings.Formatting = Formatting.Indented;
+
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
