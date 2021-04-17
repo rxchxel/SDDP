2d3f1b9d36cdc211df62028ae6005b1aa2daa9a0
mason2998 <mason2998@hotmail.com>
2021-04-17 13:07:44 +0100

    Added Armour API
    Added another API and changes the names of some others

---
+++ DnD - Campaign Dashboard v.1/DnD - Campaign Dashboard v.1/Controllers/API/WeaponsAPIController.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using DnD___Campaign_Dashboard_v._1.App_Start;
+using DnD___Campaign_Dashboard_v._1.DTOs;
+using DnD___Campaign_Dashboard_v._1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DnD___Campaign_Dashboard_v._1.Controllers.API
+{
+    public class WeaponsAPIController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public WeaponsAPIController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        //First API Method for testing this should work in thoery
+        [Authorize]
+        [HttpGet]
+        public IHttpActionResult GetWeapons()
+        {
+            var config = new MappingConfig().Configure();
+            IMapper mapper = config.CreateMapper();
+            var weapons = _context.Weapons.ToList();
+            var weaponsDtos = mapper.Map<List<WeaponDTO>>(weapons);
+            return Ok(weaponsDtos);
+        }
+    }
+}
