490821ba28fb5fba616bd90c287ec3530a431312
k0tsi0s <mynameiskotsios@gmail.com>
2021-04-19 23:47:11 +0100

    Cross Finger

---
+++ DnD - Campaign Dashboard v.1/ApiLibrary/ParentClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Web;
+
+namespace DnD___Campaign_Dashboard_v._1.Models
+{
+    public abstract class ParentClass
+    {
+        
+        public int Id { get; set; }
+
+        
+        public string Name { get; set; }
+    }
+}
--- DnD - Campaign Dashboard v.1/DnD - Campaign Dashboard v.1/Controllers/SpellsController.cs
+++ DnD - Campaign Dashboard v.1/DnD - Campaign Dashboard v.1/Controllers/SpellsController.cs
@@ -1,18 +1,84 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
+using ApiLibrary;
+using DnD___Campaign_Dashboard_v._1.Models;
 
 namespace DnD___Campaign_Dashboard_v._1.Controllers
 {
     public class SpellsController : Controller
     {
+        private SpellInfoModel spellInfoModel;
         // GET: Spells
-        [Authorize]
+        //[Authorize]
         public ActionResult Index()
         {
-            return View();
+
+            Spells spells = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://www.dnd5eapi.co/api/");
+                              
+                //HTTP GET
+                var responseTask = client.GetAsync("spells");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<Spells>();
+                    readTask.Wait();
+
+                    spells = readTask.Result;
+                }
+                else //web api sent error response 
+                {
+                    //log response status here..
+
+                    spells = new Spells();
+
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+            }
+            return View(spells.results);   
         }
+
+
+        public ActionResult Details(string id)
+        {
+
+            
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://www.dnd5eapi.co/api/");
+
+                //HTTP GET
+                var responseTask = client.GetAsync("spells/" + id);
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<SpellInfoModel>();
+                    readTask.Wait();
+
+                    spellInfoModel = readTask.Result;
+                }
+                else //web api sent error response 
+                {
+                    ////log response status here..
+
+                    //spells = new Spells();
+
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+            }
+            return View(spellInfoModel);
+        }
+
     }
 }
