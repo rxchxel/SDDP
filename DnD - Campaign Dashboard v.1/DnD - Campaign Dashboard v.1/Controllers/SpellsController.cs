d20f181322c50a2101f8f48cc1384909384969ee
mason2998 <mason2998@hotmail.com>
2021-04-20 13:07:23 +0100

    Changed the index to include datatables

--- DnD - Campaign Dashboard v.1/DnD - Campaign Dashboard v.1/Controllers/SpellsController.cs
+++ DnD - Campaign Dashboard v.1/DnD - Campaign Dashboard v.1/Controllers/SpellsController.cs
@@ -12,7 +12,71 @@ namespace DnD___Campaign_Dashboard_v._1.Controllers
         [Authorize]
         public ActionResult Index()
         {
+<<<<<<< Updated upstream
             return View();
+=======
+
+            Spells spells = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://www.dnd5eapi.co/api/");
+                              
+                //HTTP GET
+                var responseTask = client.GetAsync("spells");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<Spells>();
+                    readTask.Wait();
+
+                    spells = readTask.Result;
+                }
+                else //web api sent error response 
+                {
+                    //log response status here..
+
+                    spells = new Spells();
+
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+            }
+            return View(spells.results);   
+        }
+
+
+        public ActionResult Details(string id)
+        {
+
+            
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://www.dnd5eapi.co");
+
+                //HTTP GET
+                var responseTask = client.GetAsync(id);
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<SpellInfoModel>();
+                    readTask.Wait();
+
+                    spellInfoModel = readTask.Result;
+                }
+                else //web api sent error response 
+                {
+                    ////log response status here..
+
+                    //spells = new Spells();
+
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+            }
+            return View(spellInfoModel);
+>>>>>>> Stashed changes
         }
     }
 }
